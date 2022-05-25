12e9371a4039a9fa2d60dda1761fe85a43597e34
Serhii Bidziura <studiowebjam@gmail.com>
2022-05-25 21:38:14 +0300

    50% of ready

--- app.js
+++ app.js
@@ -16,3 +16,46 @@ function toggleMenu() {
 }
 
 toggleMenu();
+
+
+
+
+// Slider//////////////////////////////////////
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("slide-thumbnail");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  console.log(slideIndex);
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      // slides[i].style.display = "inline";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  // slides[slideIndex-1].style.display = "inline";
+  dots[slideIndex-1].className += " active";
+  
+//Adding the order number for elements in thumbnail list
+  const thumbElement = document.querySelectorAll('.slide-num');
+  for(let i = 0; i < thumbElement.length; i++){
+    thumbElement[i].innerHTML = ('00'+(i+1)).slice(-2) ;
+  };
+
+}
